4cab316e8ea56a6890f50ce59f668c9e4819bf51
Korolev, Alexey (ak3140) <ak3140@intl.att.com>
2022-02-11 15:44:51 +0200

    Migrate build configuration from Groovy to KTS (Kotlin Script)

---
+++ app/build.gradle.kts
@@ -0,0 +1,39 @@
+plugins {
+    id ("com.android.application")
+    id ("org.jetbrains.kotlin.android")
+}
+
+android {
+    defaultConfig {
+        applicationId = "net.alexandroid.template2022"
+        compileSdk = 32
+        minSdk = 21
+        targetSdk = 32
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        getByName("release") {
+            isMinifyEnabled = false
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+        }
+    }
+}
+
+dependencies {
+    // com.google
+    implementation ("com.google.android.material:material:1.5.0")
+
+    // AndroidX
+    implementation ("androidx.core:core-ktx:1.7.0")
+    implementation ("androidx.appcompat:appcompat:1.4.1")
+    implementation ("androidx.constraintlayout:constraintlayout:2.1.3")
+
+    // Tests
+    testImplementation ("junit:junit:4.13.2")
+    androidTestImplementation ("androidx.test.ext:junit:1.1.3")
+    androidTestImplementation ("androidx.test.espresso:espresso-core:3.4.0")
+}
