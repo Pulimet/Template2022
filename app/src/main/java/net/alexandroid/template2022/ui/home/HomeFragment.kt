5a2f7ef3ebb77973e2b6b83c4fcec67510336e14
Korolev, Alexey (ak3140) <ak3140@intl.att.com>
2022-02-11 19:50:43 +0200

    Rename MainFragment.kt to HomeFragment.kt and layout too

---
+++ app/src/main/java/net/alexandroid/template2022/ui/home/HomeFragment.kt
@@ -0,0 +1,10 @@
+package net.alexandroid.template2022.ui.home
+
+import androidx.fragment.app.Fragment
+import net.alexandroid.template2022.R
+import net.alexandroid.template2022.databinding.FragmentHomeBinding
+import net.alexandroid.template2022.ui.binding.FragmentBinding
+
+class HomeFragment : Fragment(R.layout.fragment_home) {
+    private val binding by FragmentBinding(FragmentHomeBinding::bind)
+}
