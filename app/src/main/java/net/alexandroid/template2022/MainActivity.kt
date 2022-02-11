cad3e032b50b46b09e15d6060887a358b0843622
Korolev, Alexey (ak3140) <ak3140@intl.att.com>
2022-02-11 16:27:10 +0200

    Enable View Binding + Add it to MainActivity.kt

--- app/src/main/java/net/alexandroid/template2022/MainActivity.kt
+++ app/src/main/java/net/alexandroid/template2022/MainActivity.kt
@@ -2,10 +2,19 @@ package net.alexandroid.template2022
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import net.alexandroid.template2022.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setViewBinding()
+    }
+
+    private fun setViewBinding() {
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
