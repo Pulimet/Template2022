2363f9bb4a7552f7f3c179ea0f475186fe20ac05
Pulimet <korolev2009@gmail.com>
2022-04-09 14:22:58 +0300

    ApiAddViewModel.kt - Add dialog adding parameter

--- app/src/main/java/net/alexandroid/template2022/ui/api/add/ApiAddFragment.kt
+++ app/src/main/java/net/alexandroid/template2022/ui/api/add/ApiAddFragment.kt
@@ -2,7 +2,9 @@ package net.alexandroid.template2022.ui.api.add
 
 import android.os.Bundle
 import android.view.View
+import androidx.appcompat.app.AlertDialog
 import androidx.fragment.app.Fragment
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import net.alexandroid.template2022.R
 import net.alexandroid.template2022.databinding.FragmentAddApiBinding
 import net.alexandroid.template2022.ui.binding.FragmentBinding
@@ -15,6 +17,8 @@ class ApiAddFragment : Fragment(R.layout.fragment_add_api), View.OnClickListener
     private val binding by FragmentBinding(FragmentAddApiBinding::bind)
     private val viewModel by viewModel<ApiAddViewModel>()
     private val navViewModel by sharedViewModel<NavViewModel>()
+
+    private var dialogAddParam: AlertDialog? = null
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
@@ -30,8 +34,27 @@ class ApiAddFragment : Fragment(R.layout.fragment_add_api), View.OnClickListener
     override fun onClick(v: View?) {
         when (v?.id) {
             R.id.fabSave -> viewModel.onSaveBtnClick()
-            R.id.fabAddParam -> viewModel.onAddParamBtnClick()
+            R.id.fabAddParam -> {
+                viewModel.onAddParamBtnClick()
+                showDialogAddParam()
+            }
             R.id.fabImport -> viewModel.onImportBtnClick()
+            R.id.btnDialogAddParam -> {
+                // TODO Pass key and value from the Dialog
+                viewModel.onDialogBtnAddParam()
+                dialogAddParam?.dismiss()
+            }
         }
+    }
+
+    // TODO Move logic to ViewModel
+    private fun showDialogAddParam() {
+        dialogAddParam = MaterialAlertDialogBuilder(requireContext())
+            .setView(R.layout.dialog_add_param)
+            .show()?.apply {
+                // TODO listen for key and value
+                // TODO on key or value change update values
+                findViewById<View>(R.id.btnDialogAddParam)?.setOnClickListener(this@ApiAddFragment)
+            }
     }
 }
--- app/src/main/java/net/alexandroid/template2022/ui/api/add/ApiAddViewModel.kt
+++ app/src/main/java/net/alexandroid/template2022/ui/api/add/ApiAddViewModel.kt
@@ -2,19 +2,24 @@ package net.alexandroid.template2022.ui.api.add
 
 import net.alexandroid.template2022.ui.base.BaseViewModel
 import net.alexandroid.template2022.ui.navigation.NavViewModel
+import net.alexandroid.template2022.utils.logs.logD
 
 class ApiAddViewModel : BaseViewModel() {
     lateinit var navViewModel: NavViewModel
 
     fun onSaveBtnClick() {
-
+        logD()
     }
 
     fun onAddParamBtnClick() {
-
+        logD()
     }
 
     fun onImportBtnClick() {
+        logD()
+    }
 
+    fun onDialogBtnAddParam() {
+        logD()
     }
 }
