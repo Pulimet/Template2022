1a99ec86fc48f5c3918057f4f428992256b98c09
Pulimet <korolev2009@gmail.com>
2022-06-02 08:19:34 +0300

    Update libraries

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -51,11 +51,11 @@ dependencies {
     implementation(project(":db"))
 
     // com.google
-    implementation("com.google.android.material:material:1.6.0")
+    implementation("com.google.android.material:material:1.6.1")
 
     // AndroidX
-    implementation("androidx.core:core-ktx:1.7.0")
-    implementation("androidx.appcompat:appcompat:1.4.1")
+    implementation("androidx.core:core-ktx:1.8.0")
+    implementation("androidx.appcompat:appcompat:1.4.2")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation("androidx.swiperefreshlayout:swiperefreshlayout:1.1.0")
     // Preferences DataStore
